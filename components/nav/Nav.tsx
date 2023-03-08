4d4cbf120b993b133099105b35457f4d4b61276d
Febzey <braydent1235@gmail.com>
2023-03-07 21:06:26 -0600

    k

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,27 +1,37 @@
+import Link from "next/link"
+import { FaSearch } from "react-icons/fa";
+
 export default function NavBar() {
     return (
-        <nav className="w-full h-20 flex items-center justify-between px-8 relative">
-            <ul className="flex flex-row gap-3 items-center justify-center text-neutral-400">
+        <nav className="w-full h-16 flex items-center justify-between px-8 relative border-b-2 border-neutral-800">
+            <ul className="flex lg:ml-24 flex-row gap-3 items-center justify-center text-neutral-400 z-10">
                 <li>Home</li>
                 <li>Create</li>
             </ul>
 
             <NavSearch />
+
+            <ul className="text-sm lg:mr-24 z-10 flex items-center justify-center">
+                <Link href={"/signin"} className="text-white px-3 py-1 bg-slate-600 border-b-[5px] border-b-slate-500 rounded cursor-pointer duration-150 hover:bg-slate-500">Sign in</Link>
+            </ul>
+
         </nav>
     )
 }
 
 const NavSearch = () => {
     return (
-        <div className="absolute text-center right-0 left-0">
-            <div className="flex items-center justify-center gap-2">
+        <div className="absolute text-center right-0 left-0 z-0">
+            <div className="flex items-center justify-center gap-2 h-8">
                 <input
                     type="text"
                     id="searchbar"
                     placeholder="Search for a post or user."
-                    className="bg-neutral-700 p-1 text-sm text-center placeholder-neutral-400 rounded text-neutral-100"
+                    className="bg-neutral-700 px-3 py-1 text-sm text-center h-full placeholder-neutral-400 rounded text-neutral-100"
                 />
-                <button className="p-1 text-sm bg-emerald-500 rounded text-black font-bold">Search</button>
+                <Link href={"/search/paramshere"} className="px-4 py-2 h-full bg-slate-600 border-b-[5px] border-b-slate-500 rounded text-neutral-100 duration-150 hover:bg-slate-500">
+                    <FaSearch />
+                </Link>
             </div>
         </div>
     )
